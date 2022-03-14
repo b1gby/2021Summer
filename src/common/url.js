e89f791d5f6df11f6f03ca2119c38d46f8ab877e
Xiangjin Zhan <674039707@qq.com>
2022-03-14 17:14:52 +0800

    优化exercise页面，更新package.json

---
+++ src/common/url.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "https://www.kaigestudy.top:8080";
+export const IMAGE_URL = BASE_URL + "/app/file/get_image?name=/";
+export const IMAGE_OF_MY_ANSWER_URL = IMAGE_URL + "my_answer/";
+export const AUDIO_URL = BASE_URL + "/app/file/get_audio?name=/";
