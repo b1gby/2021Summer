1764adecd58e757e06a931746b6158153202541d
Xiangjin Zhan <674039707@qq.com>
2022-03-18 16:01:43 +0800

    uploadFile.js生成文件地址时，对已上传文件，处理地址开头的“/”符号

--- src/common/uploadFile.js
+++ src/common/uploadFile.js
@@ -1,41 +1,79 @@
 import wepy from 'wepy';
 
-export async function uploadImageList(imageList, directoryName="/tmp"){
-  const imageUrlList = [];
-  const failedImageList = [];
-  for (let key in imageList){
-    const image = imageList[key];
-    let serverDirectory = "https://www.kaigestudy.top:8080/app/file/get_image?name=" + directoryName + "/";
-    const index = image.indexOf(serverDirectory);
-    if(index >= 0){
-      imageUrlList[key] = image.substring(index + serverDirectory.length);
-      continue;
+export async function uploadImageList(imageList, directoryName = 'tmp') {
+    const imageUrlList = [];
+    const failedImageList = [];
+    for (let key in imageList) {
+        const image = imageList[key];
+        let serverDirectory = 'https://www.kaigestudy.top:8080/app/file/get_image?name=' + directoryName;
+        const index = image.indexOf(serverDirectory);
+        if (index >= 0) {
+            const temp = image.substring(index + serverDirectory.length);
+            const array = temp.match(/\/+(.+)/);
+            imageUrlList[key] = array[1];
+            continue;
+        }
+        await new Promise((resolve) => {
+            wepy.uploadFile({
+                url: wepy.$instance.globalData.serverUrl + '/app/file/upload_file',
+                header: wepy.$instance.setHeader(),
+                filePath: image,
+                name: 'uploadFile',
+                formData: {
+                    dirName: 'images/' + directoryName
+                },
+                success(e) {
+                    const data = JSON.parse(e.data);
+                    if (data.Code == 1) {
+                        imageUrlList[key] = data.Data;
+                    } else {
+                        failedImageList.push(image);
+                    }
+                },
+                fail(e) {
+                    failedImageList.push(image);
+                },
+                complete(e) {
+                    resolve();
+                }
+            });
+        });
+    }
+    return { imageUrlList, failedImageList };
+}
+
+// 上传音频
+export async function audioUpload(audioPath, directoryName = '') {
+    let returnPath;
+    let serverDirectory = 'https://www.kaigestudy.top:8080/app/file/get_audio?name=' + directoryName;
+    const index = audioPath.indexOf(serverDirectory);
+    if (index >= 0) {
+        const temp = audioPath.substring(index + serverDirectory.length);
+        const array = temp.match(/\/+(.+)/);
+        return array[1];
     }
     await new Promise((resolve) => {
-      wepy.uploadFile({
-        url: wepy.$instance.globalData.serverUrl + '/app/file/upload_file',
-        header: wepy.$instance.setHeader(),
-        filePath:image,
-        name: 'uploadFile',
-        formData: {
-          dirName: "images/" + directoryName
-        },
-        success(e){
-          const data = JSON.parse(e.data);
-          if(data.Code == 1){
-            imageUrlList[key] = data.Data;
-          }else{
-            failedImageList.push(image);
-          }
-        },
-        fail(e){
-          failedImageList.push(image);
-        },
-        complete(e){
-          resolve();
-        }
-      })
-    })
-  }
-  return { imageUrlList, failedImageList  };
+        wepy.uploadFile({
+            url: wepy.$instance.globalData.serverUrl + '/app/file/upload_file', //开发者服务器 url
+            header: wepy.$instance.setHeader(),
+            filePath: audioPath, //要上传文件资源的路径
+            name: 'uploadFile', //文件对应的 key , 开发者在服务器端通过这个 key 可以获取到文件二进制内容
+            formData: {
+                dirName: 'audios/' + directoryName
+            },
+            success(e) {
+                const data = JSON.parse(e.data);
+                if (data.Code == 1) {
+                    returnPath = data.Data;
+                }
+            },
+            fail() {
+                console.log('录音保存失败');
+            },
+            complete() {
+                resolve();
+            }
+        });
+    });
+    return returnPath;
 }
